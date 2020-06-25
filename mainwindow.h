78389c8cc1f146668df98d00e3aec771c3d8634f
egecetin <egecetin@hotmail.com.tr>
2020-06-25 18:49:35 +0300

    gui edit

--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "filer.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -13,9 +15,20 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+	void srcPathClicked();
+	void destPathClicked();
+	void compressionMethod(int id);
+	void encryptionMethod(int id);
+	void convert();
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+	std::string pathSrc, pathDest;
+	ege::COMPRESSION_METHOD comp;
+	ege::CRYPTO_METHOD crypt;
+
+	void setPath(int id);
+
 };
 #endif // MAINWINDOW_H
